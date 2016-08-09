94840d93d35f1dc1566bff3406d1b23f4f8313ad
Samuel Loup <loup@epsitec.ch>
2016-08-09 15:27:40 +0200

    Libify server

--- bin/server.js
+++ bin/server.js
@@ -1,22 +1,6 @@
 #!/usr/bin/env node
 'use strict';
-
-const moduleName = 'goblins';
-
-require ('../lib/init-env.js');
-
-const xLog = require ('xcraft-core-log') (moduleName);
-
-const serverOptions = {
-  detached: false,
-  logs:     true,
-  response: {
-    log: xLog
-  }
-};
-
-const xServer = require ('xcraft-core-server') (serverOptions);
-
+const xServer = require ('../server.js') (require ('shrew') ());
 xServer.start ((err) => {
   if (err) {
     xLog.err (err);
