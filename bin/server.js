9b1ef6dd3faa0ee7e3f7dcb7b04727c4b621aa22
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-08-10 16:16:54 +0200

    Cosmetics: simpligy, etc, ...

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
-const root = require ('shrew') ();
+
+const root    = require ('shrew') ();
 const xServer = require ('../server.js') (root, root);
 xServer.start ((err) => {
   if (err) {
